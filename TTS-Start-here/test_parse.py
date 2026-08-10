dca23093875d593520ce0e6374a402489ff4d81b
nonx2360 <macandiso1@gmail.com>
2026-08-10 22:06:21 +0700

    clean: remove pycache from tracking, update gitignore

--- TTS-Start-here/test_parse.py
+++
@@ -1,9 +0,0 @@
-from tts.yuuna_reply import parse_gemma_output, build_style_prompt, format_display_text
-
-r = parse_gemma_output('{"text":"Hi!","emotion":"cheerful","intensity":0.9}')
-print("JSON:", r)
-print("Style:", build_style_prompt(r))
-print("Display:", format_display_text(r))
-
-r2 = parse_gemma_output("[HAPPY] plain fallback")
-print("Fallback:", r2)
